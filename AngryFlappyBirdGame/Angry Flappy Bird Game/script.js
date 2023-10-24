903f56fe75398798e1a75c58fb770c60992cb837
AnhVuong146 <99466654+AnhVuong146@users.noreply.github.com>
2023-10-23 20:45:29 -0700

    Add files via upload

---
+++ AngryFlappyBirdGame/Angry Flappy Bird Game/script.js
@@ -0,0 +1,146 @@
+let move_speed = 5, grativy = 0.5; //desktop move_speed = 3 and gravity = 0.3
+let gameTitle = document.querySelector('.game-title');
+let credit = document.querySelector('.credit');
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+let sound_point = new Audio('sounds effect/point.mp3');
+let sound_die = new Audio('sounds effect/die.mp3');
+
+// getting bird element properties
+let bird_props = bird.getBoundingClientRect();
+
+// This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+let background = document.querySelector('.background').getBoundingClientRect();
+
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
+
+//CHANGING BIRD SKINS
+let currentBirdImageIndex = 1;
+let totalBirdImages = 6;
+let birdIcon = document.querySelector('.bird-icon');
+
+birdIcon.addEventListener('click', () => {
+  currentBirdImageIndex++;
+
+  if (currentBirdImageIndex > totalBirdImages) {
+      currentBirdImageIndex = 1;
+  }
+
+  let newBirdSrc = `images/Bird-${currentBirdImageIndex}.webp`;
+  img.src = newBirdSrc; // Change the main bird's image
+  birdIcon.src = newBirdSrc; // Change the icon's image to give a preview
+});
+
+document.addEventListener('keydown', (e) => {
+    
+    if(e.key == 'Enter' && game_state != 'Play'){
+        document.querySelectorAll('.pipe_sprite').forEach((e) => {
+            e.remove();
+        });
+        img.style.display = 'block';
+        bird.style.top = '100vh';
+        game_state = 'Play';
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        message.classList.remove('messageStyle');
+        gameTitle.style.display = 'none';
+        credit.style.display = 'none';
+        play();
+    }
+});
+
+function play(){
+    function move(){
+        if(game_state != 'Play') return;
+
+        let pipe_sprite = document.querySelectorAll('.pipe_sprite');
+        pipe_sprite.forEach((element) => {
+            let pipe_sprite_props = element.getBoundingClientRect();
+            bird_props = bird.getBoundingClientRect();
+
+            if(pipe_sprite_props.right <= 0){
+                element.remove();
+            }else{
+                if(bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width && bird_props.left + bird_props.width > pipe_sprite_props.left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height && bird_props.top + bird_props.height > pipe_sprite_props.top){
+                    game_state = 'End';
+                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
+                    message.classList.add('messageStyle');
+                    img.style.display = 'none';
+                    sound_die.play();
+                    return;
+                }else{
+                    if(pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + move_speed >= bird_props.left && element.increase_score == '1'){
+                        score_val.innerHTML =+ score_val.innerHTML + 1;
+                        sound_point.play();
+                    }
+                    element.style.left = pipe_sprite_props.left - move_speed + 'px';
+                }
+            }
+        });
+        requestAnimationFrame(move);
+    }
+    requestAnimationFrame(move);
+
+    //BIRD MOVEMENT FUNCTIONS
+    let bird_dy = 0;
+    function apply_gravity(){
+        if(game_state != 'Play') return;
+        bird_dy = bird_dy + grativy;
+        document.addEventListener('keydown', (e) => {
+            if(e.key == 'ArrowUp' || e.key == ' '){
+                bird_dy = -8.6;
+            }
+        });
+
+        if(bird_props.top <= 0 || bird_props.bottom >= background.bottom){
+            game_state = 'End';
+            message.style.left = '28vw';
+            window.location.reload();
+            message.classList.remove('messageStyle');
+            return;
+        }
+        bird.style.top = bird_props.top + bird_dy + 'px';
+        bird_props = bird.getBoundingClientRect();
+        requestAnimationFrame(apply_gravity);
+    }
+    requestAnimationFrame(apply_gravity);
+
+    //PIPE CREATION
+    let pipe_seperation = 5; //desktop pipe_seperation = 10
+
+    let pipe_gap = 45; //desktop pipe_gap = 55
+
+    function create_pipe(){
+        if(game_state != 'Play') return;
+
+        if(pipe_seperation > 115){
+            pipe_seperation = 40;
+
+            //randomly generate the pipes
+            let pipe_posi = Math.floor(Math.random() * 43) + 8; 
+            let pipe_sprite_inv = document.createElement('div');
+            pipe_sprite_inv.className = 'pipe_sprite';
+            pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
+            pipe_sprite_inv.style.left = '100vw';
+
+            document.body.appendChild(pipe_sprite_inv);
+            let pipe_sprite = document.createElement('div');
+            pipe_sprite.className = 'pipe_sprite';
+            pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
+            pipe_sprite.style.left = '100vw';
+            pipe_sprite.increase_score = '1';
+
+            document.body.appendChild(pipe_sprite);
+        }
+        pipe_seperation++;
+        requestAnimationFrame(create_pipe);
+    }
+    requestAnimationFrame(create_pipe);
+}
